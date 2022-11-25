e1a60e4918832a155e7cd26103e97d7fcc64ae6c
yzzjzzzhzzz <whdgus3012@naver.com>
2022-11-25 14:31:08 +0900

    seven

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import PrintMyName from "./component/PrintMyName";
 import axios from "axios";
-import { useState } from "react";
 function App() {
   axios.defaults.baseURL = "http://192.168.0.14:5000";
   const [users, setUsers] = useState([]);
-  const getUsers = async() => {
-    const {data} = await axios.get("/user")
+  const [name, setName] = useState("");
+  const getUsers = async () => {
+    const { data } = await axios.get("/user");
     setUsers(data);
+  };
+  const postUsers = async () => {
+    const { status } = await axios.post("/user", { name });
+    if (status === 200) {
+      alert("ok");
+      setName("");
+    } else {
+      alert("err");
+    }
   };
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={getUsers}>getUsers</button>
+        <button onClick={postUsers}>postUsers</button>
+        <input onChange={(e) => setName(e.target.value)} value={name}></input>
         {users.map((el) => (
           <PrintMyName name={el.name}></PrintMyName>
         ))}
--- src/component/PrintMyName.js
+++ src/component/PrintMyName.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
-
-
-const PrintMyName = () => {
-    const [name,setName] = useState("♚♚히어로즈 오브 더 스☆톰♚♚가입시$$");
-    return <p>{name}</p>
+const PrintMyName = ({ name }) => {
+  return <p>{name}</p>;
 };
-
 export default PrintMyName;
-// 아이 야무지다
