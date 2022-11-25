3a625afa231b05dc8e0d052a10ae7e12721d9e33
yzzjzzzhzzz <whdgus3012@naver.com>
2022-11-25 12:46:31 +0900

    six

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-import './App.css';
-import PrintMyName from './component/PrintMyName';
+import logo from "./logo.svg";
+import "./App.css";
+import PrintMyName from "./component/PrintMyName";
 import axios from "axios";
-
+import { useState } from "react";
 function App() {
+  axios.defaults.baseURL = "http://192.168.0.14:5000";
   const [users, setUsers] = useState([]);
-  const getUsers = async () => {
-    const {data} = await axios.get("http://192.168.0.14:5000/user");
+  const getUsers = async() => {
+    const {data} = await axios.get("/user")
     setUsers(data);
-  }
+  };
   return (
     <div className="App">
-      <header className='App-header'>
+      <header className="App-header">
         <button onClick={getUsers}>getUsers</button>
         {users.map((el) => (
-        <PrintMyName name={el.name}></PrintMyName>
+          <PrintMyName name={el.name}></PrintMyName>
         ))}
       </header>
     </div>
   );
 }
-
 export default App;
