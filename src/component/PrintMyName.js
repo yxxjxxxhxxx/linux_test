5681f7305046c78676b446587284c967a3d31302
yzzjzzzhzzz <whdgus3012@naver.com>
2022-11-25 11:26:16 +0900

    gunho_jungil

--- src/component/PrintMyName.js
+++ src/component/PrintMyName.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 
 const PrintMyName = () => {
-    const [name,setName] = useState("이규리 흐흐흐흐흐");
+    const [name,setName] = useState("건호 ❤ 정일");
     return <p>{name}</p>
 };
 
 export default PrintMyName;
+// 아이 야무지다
