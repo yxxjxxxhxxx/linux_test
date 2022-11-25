0b393e9c8945458f2b89c36d7815bfe622c933df
yzzjzzzhzzz <whdgus3012@naver.com>
2022-11-25 11:39:04 +0900

    four

--- src/component/PrintMyName.js
+++ src/component/PrintMyName.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 const PrintMyName = () => {
-    const [name,setName] = useState("건호 ❤ 정일");
+    const [name,setName] = useState("♚♚히어로즈 오브 더 스☆톰♚♚가입시$$전원 카드팩☜☜뒷면100%증정※\n ♜월드오브 워크래프트♜펫 무료증정￥ 특정조건 §§디아블로3§§★공허의유산★초상화획득기회@@@\n 즉시이동http://kr.battle.net/heroes/ko/");
     return <p>{name}</p>
 };
 
